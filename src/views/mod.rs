a58a88936c2a3123603e5bec141a9041df888072
Edgar <git@edgarluque.com>
2021-04-05 13:41:09 +0200

    errors.html

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -7,7 +7,7 @@ pub use askama::Template;
 use derive_more::Constructor;
 
 #[derive(Template, Constructor)]
-#[template(path = "index.html")]
+#[template(path = "errors.html")]
 pub struct Error<'a> {
     pub title: &'a str,
     pub code: i32,
